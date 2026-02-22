095781a1d07be3df041a417fd0acdb05081317a2
Anurag Tiwari <tiwarianurag812@gmail.com>
2026-02-22 17:00:22 +0530

    UI fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 /* Pages */
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import ProjectsPage from "./pages/ProjectsPage";
 import Contact from "./pages/Contact";
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Portfolio />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<Contact />} />
